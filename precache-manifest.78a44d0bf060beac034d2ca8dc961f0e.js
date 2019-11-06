105c1bff8a9e2d79d6be641655428f981ed8afeb
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-06 14:18:13 +0300

    Updates

---
+++ precache-manifest.78a44d0bf060beac034d2ca8dc961f0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194915541f558017e7ecffdd9ff4e2e6",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "ef4319d038c08aa9231b",
+    "url": "/react_tabs-with-router/static/css/main.83d83705.chunk.css"
+  },
+  {
+    "revision": "a7959ae8aa8b31c59acf",
+    "url": "/react_tabs-with-router/static/js/2.9ff6dd08.chunk.js"
+  },
+  {
+    "revision": "ef4319d038c08aa9231b",
+    "url": "/react_tabs-with-router/static/js/main.d4088fcd.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
